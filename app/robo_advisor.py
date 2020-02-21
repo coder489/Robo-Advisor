f9fd46033d2cc6db3eb8e9cdd2be31aa729bd01d
coder489 <aai34@georgetown.edu>
2020-02-21 17:36:37 -0500

    collected and printed the latest day

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -15,14 +15,15 @@ request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&sym
 print("URL:", request_url)
 
 response = requests.get(request_url)
-print(type(response))
-print(response.status_code)
-print(type(response.text)) #> str
+#print(type(response)) #> response
+#print(response.status_code) #> 200
+#print(type(response.text)) #> str
 
 parsed_response = json.loads(response.text)
-print(type(parsed_response)) #> dict
 
-print(parsed_response)
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+# print(type(parsed_response)) #> dict
+
 
 
 
@@ -35,7 +36,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
