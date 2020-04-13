0def260ec99b1ad08925989d803db8fba520c0a0
coder489 <aai34@georgetown.edu>
2020-04-13 00:32:27 -0700

    Fixed url gathering test

--- test/robo_advisor_test.py
+++ test/robo_advisor_test.py
@@ -19,11 +19,11 @@ def test_to_usd():
     result = to_usd(82.9)
     assert result == "$82.90"
 
-#def test_url_gathering():
-#    result = url_gathering("tsla", "TEST_API")
-#    assert result == "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=tsla&apikey=TEST_APT"
-#
-#
+def test_url_gathering():
+    result = url_gathering("tsla", "TEST_API")
+    assert result == "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=tsla&apikey=TEST_API"
+
+
 def test_recommendation_reason():
     result = recommendation_reason(float(10), float(5))
     assert result == "Don't buy, because the current price is greater than 90% of the recent highest price, so you should wait to buy the stock until the price decreases."
